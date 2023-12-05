3d6957a5d5ef44650fa36994141b2aff031e8ff7
RakeshRoy-977 <15rakeshroy@gmail.com>
2023-12-05 19:47:51 +0530

    update

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA9N6HZ-QNCbw9VDRjF3QUKHHfdLOw1WwE",
@@ -6,8 +7,10 @@ const firebaseConfig = {
   projectId: "expense-tracker-ea863",
   storageBucket: "expense-tracker-ea863.appspot.com",
   messagingSenderId: "1032729256392",
-  appId: "1:1032729256392:web:12e37ced9151f7c33ba0fd"
+  appId: "1:1032729256392:web:12e37ced9151f7c33ba0fd",
+  databaseURL: "https://expense-tracker-ea863-default-rtdb.firebaseio.com/"
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export default app
+export const db = getFirestore(app);
--- src/Pages/ExpenseTracker.jsx
+++ src/Pages/ExpenseTracker.jsx
@@ -1,12 +1,34 @@
 import React, { useState } from 'react'
 import "./ExpenseTracker.css"
 import NavBar from '../Components/NavBar'
+import app from "../Firebase"
+import { getDatabase, ref, set, get } from "firebase/database";
+import { serverTimestamp } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { uid } from 'uid';
+
+const auth = getAuth(app);
+const db = getDatabase(app);
 
 const ExpenseTracker = () => {
     const [totalMoney,settotalMoney] =useState(0)
-    const [note,setNote] =useState("")
+    const [Note,setNote] =useState("")
     const [Money,setMoney] =useState(0)
     const [value,setValue] =useState("")
+
+
+
+
+    const witeData =()=>{
+        const UID = uid()
+        set(ref(db,`users/${auth.currentUser.uid}/${UID}`), {
+            Money,
+            Note,
+            Type:value,
+            Date: serverTimestamp(),
+            ID:UID,
+          });
+    }
   return (
     <>
     <NavBar/>
@@ -17,14 +39,18 @@ const ExpenseTracker = () => {
             <p>{totalMoney}</p>
         </div>
         <div className="TransactionsInput">
-            <input type="number" placeholder='Money' />
-            <input type="text" placeholder='Note' />
+            <input type="number" placeholder='Money' value={Money} onChange={e=>setMoney(e.target.value)}/>
+            <input type="text" placeholder='Note' value={Note} onChange={e=>setNote(e.target.value)}/>
 
             <select id="dropdown" value={value} onChange={(e)=>setValue(e.target.value)}>
                 <option value="Income">Income</option>
                 <option value="Expense">Expense</option>
             </select>
-            <button>Add</button>
+            <button onClick={witeData}>Add</button>
+        </div>
+        <div className="Transactions">
+            <p>Transactions</p>
+            
         </div>
         </div>
 
